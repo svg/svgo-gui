046566e43a13e5cc37bafc00baf6f5dc1b8139e5
rejas <github@veeck.de>
2015-07-21 23:15:34 +0200

    fixed and cleanup grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,24 @@
-/*jshint camelcase: false*/
-
 module.exports = function (grunt) {
     'use strict';
 
-    // load all grunt tasks
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
 
-    // configurable paths
-    var config = {
-        src: 'app.nw',
-        dist: 'dist'
-    };
-
     grunt.initConfig({
-
-        nodewebkit: {
+        nwjs: {
             options: {
+                mac_icns: './osx/app.icns',
                 platforms: ['osx'],
-                buildDir: './webkitbuilds' // Where the build version of my node-webkit app is saved
+                buildDir: './dist' // Where the build version of my node-webkit app is saved
             },
-            src: ['./app.nw/**/*'] // Your node-webkit app
+            files: {
+                dot: true,
+                src: ['./app.nw/**/*'] // Your node-webkit app
+            }
         }
     });
 
     grunt.registerTask('default', [
-        'nodewebkit'
+        'nwjs'
     ]);
 };
