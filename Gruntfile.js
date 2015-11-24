303fd70cfcf1af1be3d29faf15c30d88c65c62ed
Veeck <veeck@neofonie.de>
2015-11-24 12:24:14 +0100

    set version to match svgo's, dont use latest nw-alpha

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,14 @@ module.exports = function (grunt) {
     grunt.initConfig({
         nwjs: {
             options: {
+                version: '0.12.3',
                 mac_icns: './osx/app.icns',
                 platforms: ['osx', 'win'],
-                buildDir: './dist' // Where the build version of my node-webkit app is saved
+                buildDir: './dist'
             },
             files: {
                 dot: true,
-                src: ['./app.nw/**/*'] // Your node-webkit app
+                src: ['./app.nw/**/*']
             }
         }
     });
